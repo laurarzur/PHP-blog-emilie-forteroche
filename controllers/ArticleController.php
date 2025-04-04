c0f56756f6abb1606ab7965831494100c57d5b91
laurarzur <laura.arzur@gmail.com>
2025-04-04 13:40:47 +0200

    stockage et incrementation du nombre de vues

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
-class ArticleController 
+class ArticleController
 {
     /**
      * Affiche la page d'accueil.
      * @return void
      */
-    public function showHome() : void
+    public function showHome(): void
     {
         $articleManager = new ArticleManager();
         $articles = $articleManager->getAllArticles();
@@ -19,17 +19,20 @@ class ArticleController
      * Affiche le détail d'un article.
      * @return void
      */
-    public function showArticle() : void
+    public function showArticle(): void
     {
         // Récupération de l'id de l'article demandé.
         $id = Utils::request("id", -1);
 
         $articleManager = new ArticleManager();
         $article = $articleManager->getArticleById($id);
-        
+
         if (!$article) {
             throw new Exception("L'article demandé n'existe pas.");
         }
+
+        $views = $article->getViews();
+        $articleManager->incrementViews($id, $views);
 
         $commentManager = new CommentManager();
         $comments = $commentManager->getAllCommentsByArticleId($id);
@@ -42,7 +45,7 @@ class ArticleController
      * Affiche le formulaire d'ajout d'un article.
      * @return void
      */
-    public function addArticle() : void
+    public function addArticle(): void
     {
         $view = new View("Ajouter un article");
         $view->render("addArticle");
@@ -52,7 +55,8 @@ class ArticleController
      * Affiche la page "à propos".
      * @return void
      */
-    public function showApropos() {
+    public function showApropos()
+    {
         $view = new View("A propos");
         $view->render("apropos");
     }
